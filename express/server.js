68a71be7f218ef7239d41eebd812ca2fceefe3de
Joao Rosa <joao.rosa@infor.com>
2019-05-03 18:42:33 +0200

    First mongodb read

--- express/server.js
+++ express/server.js
@@ -3,14 +3,42 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://ps-user:Qs1.2011@houses-cluster-rhxbl.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+var assert = require('assert');
 
 const router = express.Router();
+
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Personalized message!</h1>');
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.get('/house', (req, res) => {
+  
+  client.connect( err => {
+    assert.equal(null, err);
+
+    var list = [];
+    var collection = client.db("test").collection("houses");
+    var houses = collection.find({}).limit(2); 
+
+    houses.forEach( (doc,err) => {
+      assert.equal(null, err);
+      list.push(doc);
+    }, () => {
+      client.close();
+      res.json(list);
+    });
+    
+    
+  });
+
+});
+
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
@@ -18,3 +46,6 @@ app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
+
+
+
